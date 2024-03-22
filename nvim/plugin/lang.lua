34d75996361af5452c34bd49b735f589e67888bf
Matias Pan <matipan@hey.com>
2024-03-22 10:45:21 -0300

    Add language specific configs

---
+++ nvim/plugin/lang.lua
@@ -0,0 +1,21 @@
+-- The following two autocmd perform auto save and autoimport for go
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = { "*.go" },
+  callback = function()
+	  local orignal = vim.notify
+	  vim.notify = function(msg, level, opts)
+		  if msg == 'No code actions available' then
+			  return
+		  end
+		  orignal(msg, level, opts)
+	  end
+
+	  vim.lsp.buf.format(nil, 3000)
+  end,
+})
+
+vim.cmd[[
+autocmd BufWritePre *.go :silent! lua vim.lsp.buf.code_action({ context = { only = { "source.organizeImports" } }, apply = true })
+autocmd BufReadPost *.zig :silent! Copilot disable
+autocmd BufWritePre *.zig :silent! lua vim.lsp.buf.format()
+]]
