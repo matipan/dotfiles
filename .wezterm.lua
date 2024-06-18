fb4b7807ee80cb24e4978485b3cd8d7d59f8606f
Matias Pan <gh@matiaspan.dev>
2024-06-18 19:00:56 -0300

    wezterm
    Signed-off-by: Matias Pan <gh@matiaspan.dev>

---
+++ .wezterm.lua
@@ -0,0 +1,19 @@
+
+local wezterm = require 'wezterm'
+
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+-- This is where you actually apply your config choices
+config.color_scheme = 'AdventureTime'
+config.color_scheme = 'Tokyo Night Storm'
+config.font = wezterm.font 'JetBrainsMono Nerd Font'
+config.dpi = 192.0
+config.font_size = 12.0
+config.hide_tab_bar_if_only_one_tab = true
+return config
