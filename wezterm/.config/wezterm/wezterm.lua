3aef23dd3b6f4ff083f867e268cd60499e7bb273
Matias Pan <gh@matiaspan.dev>
2024-09-27 09:53:28 -0300

    Proper font size
    Signed-off-by: Matias Pan <gh@matiaspan.dev>

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -12,6 +12,6 @@ end
 -- This is where you actually apply your config choices
 config.color_scheme = 'Tokyo Night Storm'
 config.font = wezterm.font 'JetBrainsMono Nerd Font'
-config.font_size = 12.0
+config.font_size = 14.0
 config.hide_tab_bar_if_only_one_tab = true
 return config
