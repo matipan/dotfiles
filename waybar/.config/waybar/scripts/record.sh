5dfe4dcbe017515f22359f4a528c2d4abdaace24
Matias Pan <gh@matiaspan.dev>
2024-10-21 09:49:06 -0300

    Setup recording button on waybar
    Signed-off-by: Matias Pan <gh@matiaspan.dev>

---
+++ waybar/.config/waybar/scripts/record.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+OUTPUT_FILE="/home/matipan/Videos/$(date +%Y-%m-%d-%T).mp4"
+
+if pgrep -x "wf-recorder" > /dev/null; then
+    # If wf-recorder is running, stop it
+    pkill -SIGINT wf-recorder
+    sleep 0.3  # Wait for 300ms
+    notify-send "Screen Recording" "Saved recording to $OUTPUT_FILE"
+else
+    # If wf-recorder is not running, start it
+    GEOMETRY=$(slurp)
+    if [ -n "$GEOMETRY" ]; then
+        wf-recorder -g "$GEOMETRY" -f "$OUTPUT_FILE" &
+        notify-send "Screen Recording" "Recording started"
+    else
+        notify-send "Screen Recording" "Selection cancelled"
+    fi
+fi
